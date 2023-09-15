ece8b6c82f92a65151b44ccc9e8b1830cd01e9f9
Rsam <arsam.hb@gmail.com>
2023-09-15 20:25:36 +0330

    sprint 1 done

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 function makeMovingCircles() {
+  const screenWidth = $(window).width();
+  let top = "-25px";
+  let left = "47.5vw";
   let movingObjs = "";
-
+  numberForimage = 1;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 5; j++) {
       style = `  
         position: absolute;
-        left: 47.5vw;
-        top: -25px;
+        top: ${top};
+        left: ${left};
         width: 50px;
         height: 50px;
+        border-radius : 100%
         margin: auto;
-        animation: circular-movement-${i + 1}-${j + 1} 40s infinite linear;
-        background-image: url('./img/circle${j + i + 1}.png');
+        animation: circular-movement-${i + 1}-${j + 1} ${
+        i == 0 ? "10s" : i == 1 ? "10s" : "10s"
+      } infinite linear;
+        animation-direction: ${i == 1 ? "reverse" : "normal"}; 
+        background-image: url('./img/circle${
+          numberForimage == 15 ? 5 : numberForimage
+        }.png');
         background-repeat: no-repeat;
         background-position: center;`;
 
       movingObjs += `<div style="${style}"></div>`;
+      numberForimage++;
     }
   }
   $("#circles-cotainer").append(movingObjs);
@@ -36,4 +46,5 @@ animation += `@keyframes circular-movement-${i + 1}-${j + 1} {
         30 + i * 7.5
       }vw) rotate(-${360 - 360 * (j / 5) + i * 25}deg);
     }
-} ---- `;  */
+} ---- `;  
+*/
